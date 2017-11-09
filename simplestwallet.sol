546eeb38be6148be24ea6509b60bb362f704bd0c
Christoph Mussenbrock <christoph@etherisc.com>
2017-11-09 14:26:51 +0100

    Fix Array Indices.

--- simplestwallet.sol
+++ simplestwallet.sol
@@ -19,9 +19,9 @@ contract SimplestWallet {
         // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
         bytes32 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce);
     
-        if (owner1 == ecrecover(txHash, sigV[1], sigR[1], sigS[1])) recovered++; 
-        if (owner2 == ecrecover(txHash, sigV[2], sigR[2], sigS[2])) recovered++; 
-        if (owner3 == ecrecover(txHash, sigV[3], sigR[3], sigS[3])) recovered++; 
+        if (owner1 == ecrecover(txHash, sigV[0], sigR[0], sigS[0])) recovered++; 
+        if (owner2 == ecrecover(txHash, sigV[1], sigR[1], sigS[1])) recovered++; 
+        if (owner3 == ecrecover(txHash, sigV[2], sigR[2], sigS[2])) recovered++; 
         
         require(recovered >= threshold);
         nonce = nonce + 1;
