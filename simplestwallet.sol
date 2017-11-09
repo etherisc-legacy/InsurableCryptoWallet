62509ed7d8537b269de7c180d3e01d08e3408910
Christoph Mussenbrock <christoph@etherisc.com>
2017-11-09 14:42:54 +0100

    Align owner numbering

--- simplestwallet.sol
+++ simplestwallet.sol
@@ -2,9 +2,9 @@ pragma solidity 0.4.18;
 
 contract SimplestWallet {
     
-    address constant public owner1 = 0x0;
-    address constant public owner2 = 0x0;
-    address constant public owner3 = 0x0;
+    address constant public owner0 = 0xdeaddeaddeaddeaddeaddeaddeaddeaddeaddead; // hardcode addresses at time of deployment.
+    address constant public owner1 = 0xcafecafecafecafecafecafecafecafecafecafe;
+    address constant public owner2 = 0xbeefbeefbeefbeefbeefbeefbeefbeefbeefbeef;
     uint8 constant public threshold = 2;
     uint256 public nonce;
     
@@ -19,9 +19,9 @@ contract SimplestWallet {
         // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
         bytes32 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce);
     
-        if (owner1 == ecrecover(txHash, sigV[0], sigR[0], sigS[0])) recovered++; 
-        if (owner2 == ecrecover(txHash, sigV[1], sigR[1], sigS[1])) recovered++; 
-        if (owner3 == ecrecover(txHash, sigV[2], sigR[2], sigS[2])) recovered++; 
+        if (owner0 == ecrecover(txHash, sigV[0], sigR[0], sigS[0])) recovered++; 
+        if (owner1 == ecrecover(txHash, sigV[1], sigR[1], sigS[1])) recovered++; 
+        if (owner2 == ecrecover(txHash, sigV[2], sigR[2], sigS[2])) recovered++; 
         
         require(recovered >= threshold);
         nonce = nonce + 1;
