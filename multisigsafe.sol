96e49d0ead89cf957083726699470bf23387358c
Christoph Mussenbrock <christoph@etherisc.com>
2017-11-11 11:51:11 +0100

    Comment alinged

--- multisigsafe.sol
+++ multisigsafe.sol
@@ -16,12 +16,12 @@ contract SimplestWallet {
     function execute(uint8 sigV1, uint8 sigV2, uint8 sigV3, bytes32 sigR1, bytes32 sigR2, bytes32 sigR3, bytes32 sigS1, bytes32 sigS2, bytes32 sigS3, uint8 destinationNumber, uint value) public {
 
         uint8 recovered;
-        // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
         if (destinationNumber == 1) { destination = destination1 }
         else if (destinationNumber == 2) { destination = destination2 }
         else if (destinationNumber == 3) { destination = destination3 }
         else throw;
 
+        // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
         bytes32 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce);
     
         if (owner1 == ecrecover(txHash, sigV1, sigR1, sigS1)) recovered++; 
