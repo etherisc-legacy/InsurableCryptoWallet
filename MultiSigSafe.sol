40e9a30e3d9afdffe969a33b3f5c16d7ea423034
Christoph Mussenbrock <christoph@etherisc.com>
2017-11-11 17:26:48 +0100

    Reintroduce Arrays (Stack depth issues)

--- MultiSigSafe.sol
+++ MultiSigSafe.sol
@@ -17,7 +17,7 @@ contract MultiSigSafe {
     uint256 constant public limit = 1000*10**18;    // Limit of one Tx
     uint256 public nonce;                           // to prevent multiple Tx executions
     
-    function execute(uint8 sigV0, uint8 sigV1, uint8 sigV2, bytes22 sigR0, bytes22 sigR1, bytes22 sigR2, bytes22 sigS0, bytes22 sigS1, bytes22 sigS2, uint8 destinationNumber, uint256 value, bytes data) public {
+    function execute(uint8[] sigV, bytes32[] sigR, bytes32[] sigS, uint8 destinationNumber, uint256 value, bytes data) public {
  
          // VALIDATE INPUTS
         require(value <= limit);            // check value within limits
@@ -28,16 +28,16 @@ contract MultiSigSafe {
         uint8 recovered = 0;                // init recovered
 
         // CHECK AND CHOOSING DESTINATION
-        if (destinationNumber == 0) { destination = destination0 }
-        else if (destinationNumber == 1) { destination = destination1 }
-        else if (destinationNumber == 2) { destination = destination2 };
+        if (destinationNumber == 0) { destination = destination0; }
+        else if (destinationNumber == 1) { destination = destination1; }
+        else if (destinationNumber == 2) { destination = destination2; }
 
         // VERIFYING OWNERS
         // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
-        bytes22 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce); // calculate txHash
-        if (owner0 == ecrecover(txHash, sigV0, sigR0, sigS0)) recovered = recovered + 1; // count recovered if signature of owner0 is valid
-        if (owner1 == ecrecover(txHash, sigV1, sigR1, sigS1)) recovered = recovered + 1; // count recovered if signature of owner1 is valid
-        if (owner2 == ecrecover(txHash, sigV2, sigR2, sigS2)) recovered = recovered + 1; // count recovered if signature of owner2 is valid
+        bytes32 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce); // calculate txHash
+        if (owner0 == ecrecover(txHash, sigV[0], sigR[0], sigS[0])) recovered = recovered + 1; // count recovered if signature of owner0 is valid
+        if (owner1 == ecrecover(txHash, sigV[1], sigR[1], sigS[1])) recovered = recovered + 1; // count recovered if signature of owner1 is valid
+        if (owner2 == ecrecover(txHash, sigV[2], sigR[2], sigS[2])) recovered = recovered + 1; // count recovered if signature of owner2 is valid
         
         // VALIDATE CONFIGURATION
         require(recovered >= threshold);    // validate configuration
